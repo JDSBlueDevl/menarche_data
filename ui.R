29a8c41d3eeb3dd678039ff351d55074a2aa92f8
JDSBlueDevl <JDSBlueDevl@gmail.com>
2016-11-16 14:45:35 -0500

    Added explanatory tab to Shiny app.

--- ui.R
+++ ui.R
@@ -8,9 +8,20 @@ shinyUI(fluidPage(
                         9, 18, value = 12)
         ),
         mainPanel(
-            plotOutput("plot1"),
-            h3("Cumulative probability of menarche:"),
-            textOutput("pred1")
-        )
+            tabsetPanel(type = "tabs",
+                 tabPanel("Data", br(),        
+                    plotOutput("plot1"),
+                    h3("Cumulative probability of menarche:"),
+                    textOutput("pred1")
+                ),
+                tabPanel("Documentation", br(), p("This is a Shiny app for the course 
+                        project in Developing Data Products. The data is based 
+                        on a study of adolescent girls in Warsaw in 1965 and 
+                        at what age they had already achieved menarche. Use the
+                        slider to choose an age, and the output will generate
+                        the probability that the girl had achieved menarche."),
+                         br(),
+                        p("Source code for server.R and ui.R are available at 
+                          https://www.github.com/JDSBlueDevl/menarche_data"))
     )
-))
+))))
